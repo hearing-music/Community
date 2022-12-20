84cf2dd440112854e8ab2ad4968e9cab2cd01f80
WangSiqi <770512544@qq.com>
2022-12-20 12:01:11 +0800

    wsq

--- src/app/pages/lsdd/template/template.component.ts
+++ src/app/pages/lsdd/template/template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,SimpleChanges} from '@angular/core';
 import {CommonService} from "../../../services/common.service";
 @Component({
   selector: 'ngx-template',
@@ -8,18 +8,45 @@ import {CommonService} from "../../../services/common.service";
 export class TemplateComponent implements OnInit {
 	@Input() list: any;
   constructor(public common: CommonService) { }
-
   ngOnInit(): void {
   }
 	audioSrc = '';
 	playAudio(url:string,i:number){
-		this.list.forEach((item:any,index:number)=>{
-			if(index == i){
-				item.isPlay = !item.isPlay
-			}else{
-				item.isPlay = false;
-			}
-		})
 		this.audioSrc = url;
+		let audio:any = document.getElementById('audio')
+		setTimeout(()=>{
+			this.list.forEach((item:any,index:number)=>{
+				if(index == i){
+					item.isPlay = !item.isPlay
+					if(item.isPlay){
+						audio.play()
+					}else{
+						audio.pause()
+					}
+				}else{
+					item.isPlay = false;
+				}
+			})
+		},50)
+		
+	}
+	play(element:any){
+		console.log(element.srcElement.currentSrc)
+		var i = this.list.findIndex((e:any)=>e.mp3url == element.srcElement.currentSrc)
+		if(i !== -1){
+			this.list[i].isPlay = true;
+		}
+	}
+	pause(){
+		this.list.forEach((item:any)=>{
+			item.isPlay = false;
+		})
+	}
+	// 切换回来 哪首歌正在播放 显示播放
+	ngOnChanges(change: SimpleChanges) {
+		var i = change.list.currentValue.findIndex((e:any)=>e.mp3url == this.audioSrc)
+		if(i !== -1){
+			this.list[i].isPlay = true;
+		}
 	}
 }
