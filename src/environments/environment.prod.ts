7c77ab3f6b9cb1a13d49cfb1dc59a2a3d9c2e6b1
WangSiqi <770512544@qq.com>
2023-01-13 14:40:01 +0800

    wsq

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: true,
 	downloadUrl:'http://139.224.133.131:3223',
-	baseUrl:'http://api.tingjianmusic.cn',
+	baseUrl:'http://communities.tingjianmusic.cn',
 	tencentUrl:'https://tcb-3e8ebbnm0ab0c7-9ddrxa0a7ebcd.service.tcloudbase.com'
 };
