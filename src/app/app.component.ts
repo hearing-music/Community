9e86ea2244a57b599139130757b33661caf96c5b
WangSiqi <770512544@qq.com>
2023-11-09 16:55:30 +0800

    wsq

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 // import { AnalyticsService } from './@core/utils/analytics.service';
 // import { SeoService } from './@core/utils/seo.service';
 
@@ -15,9 +16,14 @@ export class AppComponent implements OnInit {
 
   // constructor(private analytics: AnalyticsService, private seoService: SeoService) {
   // }
-constructor() {
+constructor(private meta: Meta) {
   }
   ngOnInit(): void {
+		// https协议 强制转换https
+		if(window.location.protocol=='https:'){
+			this.meta.addTag({ httpEquiv: 'Content-Security-Policy', content: 'upgrade-insecure-requests'});
+		}
+		
     // this.analytics.trackPageViews();
     // this.seoService.trackCanonicalChanges();
   }
