435d1eb78ac9540ad2e1c47ccfc83367a2532e01
WangSiqi <770512544@qq.com>
2024-04-17 21:39:30 +0800

    wsq

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,6 @@ export const environment = {
 	// downloadUrl:'http://192.168.43.44:3222',
 	// baseUrl:'http://localhost:3222',
 	baseUrl:'http://192.168.0.53:3222',
-	// baseUrl:'http://192.168.43.44:3222',
 	tencentUrl:'https://tcb-3e8ebbnm0ab0c7-9ddrxa0a7ebcd.service.tcloudbase.com',
 	// socketUrl:'http://127.0.0.1:3222',
 	socketUrl:'http://192.168.0.53:3222'
