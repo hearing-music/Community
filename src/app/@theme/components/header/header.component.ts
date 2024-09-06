e02cca01e505ca024ad46b7ac7ca4062d24fd28f
WangSiqi <770512544@qq.com>
2024-09-06 14:16:17 +0800

    1

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -88,7 +88,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 				color: 'bg-azure-95',
 				title: '2024年9月6日18:00',
 			  	newAdd:['qq搜索 酷狗v3搜索 酷我搜索 网易云搜索 新增封面','qq搜索 新增 可选是否查询历史上游 默认开启'],//新增
-			  	optimize:['qq搜索 酷狗v3搜索 酷我搜索 网易云搜索 每页5条->10条'],//优化
+			  	optimize:['qq搜索 酷狗v3搜索 酷我搜索 网易云搜索 每页5条->10条','抖音监控声源 增长量从高到低排序'],//优化
 			  	bugs:['修复多版本歌曲播放及多版本封面'],//修复bug
 			},
   		  {
