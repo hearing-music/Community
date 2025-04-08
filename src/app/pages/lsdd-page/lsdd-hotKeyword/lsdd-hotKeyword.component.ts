1796d89750c8187275e4160f8d0244530eb512bd
王思齐 <770512544@qq.com>
2025-04-08 12:44:10 +0800

    铃声多多页面

--- src/app/pages/lsdd-page/lsdd-hotKeyword/lsdd-hotKeyword.component.ts
+++ src/app/pages/lsdd-page/lsdd-hotKeyword/lsdd-hotKeyword.component.ts
@@ -7,5 +7,44 @@ import { Component, OnInit, Input } from "@angular/core";
 export class LsddKeywordComponent implements OnInit {
   @Input() list: any;
   constructor() {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.list);
+  }
+  selectItem = "上升";
+  tagList = [
+    {
+      name: "上升",
+    },
+    {
+      name: "下降",
+    },
+    {
+      name: "未变",
+    },
+    {
+      name: "新上",
+    },
+  ];
+  onSelect(item: any) {
+    this.selectItem = item.name;
+    if (item.name == "上升") {
+      this.list.trend = this.list.hot.filter(
+        (item: any) => item.trend && Number(item.trend) > 0
+      );
+    } else if (item.name == "下降") {
+      this.list.trend = this.list.hot.filter(
+        (item: any) => item.trend && Number(item.trend) < 0
+      );
+    } else if (item.name == "新上") {
+      this.list.trend = this.list.hot.filter(
+        (item: any) => item.trend == "new"
+      );
+    } else if (item.name == "未变") {
+      this.list.trend = this.list.hot.filter((item: any) => !item.trend);
+    }
+    this.list.trend.forEach((item: any) => {
+      item.sort = this.list.hot.findIndex((e: any) => e.txt == item.txt);
+    });
+    console.log(this.list.trend);
+  }
 }
